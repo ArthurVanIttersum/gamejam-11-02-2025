5fe2267675edcc67d18a0d57f2ed1001fd3ff79f
arthur <ajcvanittersum@gmail.com>
2025-02-11 16:14:11 +0100

    added upgrade system code

--- GameJam-11-2-2015/Assets/ResourceGeneration.cs
+++ GameJam-11-2-2015/Assets/ResourceGeneration.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 using System.Collections;
 using UnityFundamentals;
+using System;
 
 public class ResourceGeneration : MonoBehaviour
 {
     private IEnumerator coroutine;
     public IntVariable []productVariables;
-    public float[] delays = {10,5,2,1};//product generation speed should be diffined here!
+    public float[] delays = {1,2,5,10};//product generation speed should be diffined here!
+    public float[] PassiveIncomeUpgrades = {0,0,0,0 };
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,20 +22,25 @@ public class ResourceGeneration : MonoBehaviour
         //maybe update display
     }
 
-    public void InitiatePasiveIncome(int ProductIndex)
+    public void InitiatePasiveIncome(int productIndex)
     {
-        coroutine = Spawner(productVariables[ProductIndex], delays[ProductIndex]);
+        coroutine = Spawner(productVariables[productIndex], delays[productIndex], productIndex);
         StartCoroutine(coroutine);
         print("Coroutine started");
     }
 
-    private IEnumerator Spawner(IntVariable variable, float delay)
+    private IEnumerator Spawner(IntVariable variable, float delay, int productIndex)
     {
         while (true)
         {
-            yield return new WaitForSeconds(delay);
+            yield return new WaitForSeconds(delay * (float)Math.Pow(0.9f, PassiveIncomeUpgrades[productIndex]));
             variable.ChangeValueBy(1);
         }
+    }
+
+    public void UpgradePasiveIncome(int productIndex)
+    {
+        PassiveIncomeUpgrades[productIndex]++;
     }
 
 }
