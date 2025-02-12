6ed8c6a0de4d922a91f5128e75fe5d0daac7cc23
arthur <ajcvanittersum@gmail.com>
2025-02-12 13:41:09 +0100

    tested the code and fixed the bugs

--- GameJam-11-2-2015/Assets/Scripts/GoldDisplay.cs
+++ GameJam-11-2-2015/Assets/Scripts/GoldDisplay.cs
@@ -7,37 +7,43 @@ public class GoldDisplay : MonoBehaviour
     public GameObject[] goldPots;
     private GameObject currentGoldPot;
     public Vector3 goldPotPosition;
-    public int currentValue;
+    public int currentValue = 10;
     public int[] thresholds;
-    
+
+    void Start()
+    {
+        TryUpdateGoldDisplay();
+        //SwitchModel(0);
+    }
+
     public void TryUpdateGoldDisplay()
     {
         int newValue = 0;
-        if (money.GetValue() < thresholds[0])
+        if (money.GetValue() > thresholds[0])
         {
             newValue = 1;
         }
-        else if (money.GetValue() < thresholds[1])
+        if (money.GetValue() > thresholds[1])
         {
             newValue = 2;
         }
-        else if (money.GetValue() < thresholds[2])
+        if (money.GetValue() > thresholds[2])
         {
             newValue = 3;
         }
-        else if (money.GetValue() < thresholds[3])
+        if (money.GetValue() > thresholds[3])
         {
             newValue = 4;
         }
-        else if (money.GetValue() < thresholds[4])
+        if (money.GetValue() > thresholds[4])
         {
             newValue = 5;
         }
-        else if (money.GetValue() < thresholds[5])
+        if (money.GetValue() > thresholds[5])
         {
             newValue = 6;
         }
-        else if (money.GetValue() < thresholds[6])
+        if (money.GetValue() > thresholds[6])
         {
             newValue = 7;
         }
@@ -46,6 +52,7 @@ public class GoldDisplay : MonoBehaviour
         {
             SwitchModel(newValue);
         }
+        currentValue = newValue;
     }
 
     private void SwitchModel(int goldDisplayValue)
--- GameJam-11-2-2015/Assets/Scripts/ProductTextChange.cs
+++ GameJam-11-2-2015/Assets/Scripts/ProductTextChange.cs
@@ -1,13 +1,16 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityFundamentals;
 
 public class ProductTextChange : MonoBehaviour
 {
     public IntVariable product;
     public TextMeshProUGUI productText;
+    public UnityEvent unityEvent;
     public void Update()
     {
         productText.text = "Coins: " + product.GetValue();
+        unityEvent.Invoke();
     }
 }
